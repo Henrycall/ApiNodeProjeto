f31bcdff593a87858c8484f63353174613aeb8e2
Henrycall <henrycalleraa@gmail.com>
2021-08-10 17:47:02 -0300

    criacao de api

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,11 +1,20 @@
+const atendimentos = require('../models/atendimentos')
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-app.get('/atendimento',(req,res)=> res.send('rota de atendimentos estou usando um get'))
+app.get('/atendimento',(req,res)=> {
+    Atendimento.lista(res)
+})
+
+app.get('/atendimento/:id',(req,res)=>{
+    const  id = parseInt(req.params.id)
+
+    Atendimento.buscaPorID(id,res)
+})
 
 app.post('/atendimento',(req,res) => {
 const atendimento = req.body
-Atendimento.adiciona(atendimento)
-res.send('VOCE ESTA USANDO O POST')
+Atendimento.adiciona(atendimento,res)
+
 })
 }
